89f64d49ce82b9a9ec41b0000788d9c7b2d3f1d3
Aaron Bockelie <aaronsb@gmail.com>
2026-01-26 19:53:15 -0600

    Release v0.6.7: Corrected non-registering migration: 041_job_permissions.sql

--- operator.sh
+++ operator.sh
@@ -2,7 +2,7 @@
 # ============================================================================
 # operator.sh - Knowledge Graph Platform Manager (Thin Shim)
 # ============================================================================
-OPERATOR_VERSION="0.6.6"
+OPERATOR_VERSION="0.6.7"
 # ============================================================================
 #
 # Minimal host-side script that delegates to operator container.
